c866b8c9c29a1ca62e1ca930a5349cabef2f91e5
William Emfinger <waemfinger@gmail.com>
2026-01-31 16:50:54 -0600

    chore: Update project for UE 5.7 (#44)

--- UntitledForestGame/Source/UntitledForestGameEditor.Target.cs
+++ UntitledForestGame/Source/UntitledForestGameEditor.Target.cs
@@ -8,7 +8,7 @@ public class UntitledForestGameEditorTarget : TargetRules
 	public UntitledForestGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "UntitledForestGame" } );
 	}
